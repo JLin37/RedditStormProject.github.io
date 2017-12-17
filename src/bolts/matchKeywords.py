d225c7bdade1705e6489a0842908c02bf5912607
Jason Lin <haohao.jasonlin@gmail.com>
2017-12-17 15:05:04 -0500

    fixed streaming data from reddit

--- src/bolts/matchKeywords.py
+++ src/bolts/matchKeywords.py
@@ -28,35 +28,35 @@ class matchKeywordsBolt(Bolt):
         for companyName in companyNames:
             if companyName == splitTitle:
                 #sent email to company support
-                #self.sendEmail(companyName, redditTitle, redditLink)
+                self.sendEmail(companyName, redditTitle, redditLink)
                 self.logger.info("matched company [{:}]".format(companyName))
                 self.logger.info("Reddit Title [{:}]".format(redditTitle))
                 self.logger.info("Reddit Link [{:}]".format(redditLink))
                 self.emit([redditTitle, redditLink])
 
-    # def sendEmail(self, companyName, redditTitle, redditLink):
-    #     fromaddr = "arslincars@gmail.com"
-    #     toaddr = "mlin48@fordham.edu"
-    #     msg = MIMEMultipart()
-    #     msg['From'] = fromaddr
-    #     msg['To'] = toaddr
-    #     msg['Subject'] = "Hey" + companyName + "There is a reddit post about your company"
+    def sendEmail(self, companyName, redditTitle, redditLink):
+        fromaddr = "arslincars@gmail.com"
+        toaddr = "mlin48@fordham.edu"
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = "Hey" + companyName + "There is a reddit post about your company"
 
-    #     body = (" Dear Sir/Madam \n" +
-    #         " \n" +
-    #         "While our bot searched through posts on reddit, we noticed a post" +
-    #         "\n about your company." +
-    #         "\n" +
-    #         " \n Maybe you would like to look at it and see what people are saying" +
-    #         "\n about your comapny" +
-    #         "\n Here is the Post Title: " + redditTitle +
-    #         "\n and Post link: " + redditLink
-    #         )
-    #     msg.attach(MIMEText(body, 'plain'))
+        body = (" Dear Sir/Madam \n" +
+            " \n" +
+            "While our bot searched through posts on reddit, we noticed a post" +
+            "\n about your company." +
+            "\n" +
+            " \n Maybe you would like to look at it and see what people are saying" +
+            "\n about your comapny" +
+            "\n Here is the Post Title: " + redditTitle +
+            "\n and Post link: " + redditLink
+            )
+        msg.attach(MIMEText(body, 'plain'))
 
-    #     server = smtplib.SMTP('smtp.gmail.com', 587)
-    #     server.starttls()
-    #     server.login(fromaddr, "slowmotion")
-    #     text = msg.as_string()
-    #     server.sendmail(fromaddr, toaddr, text)
-    #     server.quit()
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, "slowmotion")
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
--- testingRedditAPI.py
+++ testingRedditAPI.py
@@ -14,25 +14,25 @@
 # 	print(redditStream())
 # printStream()
 
-import praw, logging
+# import praw, logging
 
-from multiprocessing import Process, Queue
+# from multiprocessing import Process, Queue
 
-def redditStream():
-	#setup reddit client info for api access
-	reddit = praw.Reddit(client_id='UwZjWiAJlqse6w',
-	                     client_secret='-c7qxVg7w8K3_FLG-lQip-b9npY',
-	                     user_agent='StormFinalProject testing')
+# def redditStream():
+# 	#setup reddit client info for api access
+# 	reddit = praw.Reddit(client_id='UwZjWiAJlqse6w',
+# 	                     client_secret='-c7qxVg7w8K3_FLG-lQip-b9npY',
+# 	                     user_agent='StormFinalProject testing')
 
-	#iterate through new reddit submission on /r/all 
-	submissions = reddit.subreddit('all').stream.submissions()
-	submission = next(submissions)
-	redditTitle = submission.title
-	print(redditTitle)
-	logging.info("Reddit Title [{:}]".format(redditTitle))
+# 	#iterate through new reddit submission on /r/all 
+# 	submissions = reddit.subreddit('all').stream.submissions()
+# 	submission = next(submissions)
+# 	redditTitle = submission.title
+# 	print(redditTitle)
+# 	logging.info("Reddit Title [{:}]".format(redditTitle))
 
 
-redditStream()
+# redditStream()
 
 # def printStream():
 # 	while True:
